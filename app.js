127d59717aa3fcdc678c0f190723878d66d0615b
eren-dabanlioglu <eren.dabanlioglu@pl.ibm.com>
2017-11-09 15:33:19 +0100

    clean

--- app.js
+++ app.js
@@ -30,30 +30,3 @@ app.get("/board", function (req, res) {
 app.listen(DEFAULT_PORT, function () {
   console.log('Example app running on port ' + DEFAULT_PORT);
 });
-
-// if (isDevelopment) {
-//   // configure webpack-dev-server
-//   var config = require('../../webpack.client.config');
-//   console.log('config.entry- ', config.entry);
-//   var webpackDevServerPort = DEFAULT_PORT + 10;
-//   config.entry.board.unshift('webpack-dev-server/client?http://localhost:' + DEFAULT_PORT + '/', 'webpack/hot/only-dev-server');
-//
-//   new WebpackDevServer(webpack(config), {
-//     hot: true,
-//     historyApiFallback: true,
-//     publicPath: "/",
-//     proxy: {
-//       '*': 'http://localhost:' + DEFAULT_PORT
-//     }
-//   }).listen(webpackDevServerPort, 'localhost', function (err, result) {
-//     if (err) {
-//       console.error(err);
-//     }
-//     console.info('Listening at localhost (WebpackDevServer):' + webpackDevServerPort);
-//   });
-// } else{
-//   var http = require("http");
-//   setInterval(function() {
-//       http.get("http://quantxy.herokuapp.com");
-//   }, 300000); // every 5 minutes (300000)
-// }
